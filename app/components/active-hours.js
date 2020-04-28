1c7f11b6ec17faeb2f986db9e878524e4cc7baeb
Lingjia Liu <gmutoo@gmail.com>
2020-04-29 00:32:36 +1000

    add active hours component

---
+++ app/components/active-hours.js
@@ -0,0 +1,156 @@
+import PropTypes from 'prop-types';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Flex, Box, Text } from 'rebass';
+import { getMonth, startOfToday, differenceInSeconds } from 'date-fns';
+import { ALL_HOURS, ALL_MONTHS } from 'utils/data';
+import Heading from './heading';
+
+const ActiveHours = ({
+  availableMonths = ALL_MONTHS,
+  availableHours = ALL_HOURS,
+  ...props
+}) => {
+  const currentMonth = getMonth(new Date());
+  const onMonth = availableMonths[currentMonth];
+  const [timeOffset, setTimeOffset] = useState(0);
+  const tick = useMemo(
+    () => () => {
+      const today = startOfToday();
+      const offset =
+        (differenceInSeconds(new Date(), today) / (24 * 60 * 60)) * 100;
+      setTimeOffset(offset);
+    },
+    [],
+  );
+  useEffect(() => {
+    tick();
+    const timer = setInterval(tick, 60 * 1000);
+    return () => clearInterval(timer);
+  }, []);
+  return (
+    <Box {...props}>
+      <Heading>Current Active Hours</Heading>
+      <Box sx={{ position: 'relative', opacity: onMonth ? '1' : '0.5' }}>
+        <Flex sx={{ transform: 'translate(-0.4ex)' }}>
+          <Text flex="0 0 50%">AM</Text>
+          <Text flex="0 0 50%">PM</Text>
+        </Flex>
+        <Flex sx={{ transform: 'translate(-0.4ex)' }}>
+          <Text flex="0 0 25%">12</Text>
+          <Text flex="0 0 25%">6</Text>
+          <Text flex="0 0 25%">12</Text>
+          <Text flex="0 0 25%">6</Text>
+        </Flex>
+        {/* ruler */}
+        <Flex
+          alignItems="flex-end"
+          sx={{
+            borderRight: '1px solid',
+            borderRightColor: 'grey-66',
+            borderBottom: '2px solid',
+            borderBottomColor: 'grey-66',
+            position: 'relative',
+            zIndex: 1,
+          }}
+        >
+          {ALL_HOURS.map((_, h) => (
+            <Box
+              sx={{
+                flex: '1 1 auto',
+                width: '4%',
+                height: '10px',
+                borderLeft: '1px solid',
+                borderLeftColor: 'grey-66',
+                '&:nth-of-type(4), &:nth-of-type(10), &:nth-of-type(16), &:nth-of-type(22)': {
+                  height: '15px',
+                },
+                '&:nth-of-type(1), &:nth-of-type(7), &:nth-of-type(13), &:nth-of-type(19)': {
+                  height: '20px',
+                },
+              }}
+              /* eslint-disable-next-line react/no-array-index-key */
+              key={h}
+            />
+          ))}
+        </Flex>
+        {/* indicator */}
+        {onMonth && (
+          <>
+            <Flex
+              sx={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                paddingBottom: '1px',
+                bottom: '3px',
+                borderBottom: '1px solid',
+                borderBottomColor: 'grey-99',
+                flexWrap: 'wrap',
+              }}
+            >
+              {ALL_HOURS.map((_, h) => (
+                <Box
+                  sx={{
+                    flex: '1 1 auto',
+                    width: '4%',
+                    height: '10px',
+                    backgroundColor: availableHours[h]
+                      ? '#b8d252'
+                      : 'transparent',
+                    borderRadius: () => {
+                      if (h === 0 || !availableHours[h - 1]) {
+                        return '5px 0 0 5px';
+                      }
+                      if (h === 23 || !availableHours[h + 1]) {
+                        return '0 5px 5px 0';
+                      }
+                      return '0';
+                    },
+                  }}
+                  /* eslint-disable-next-line react/no-array-index-key */
+                  key={h}
+                />
+              ))}
+            </Flex>
+            <Box
+              sx={{
+                height: '25px',
+                width: '2px',
+                backgroundColor: 'red',
+                position: 'absolute',
+                bottom: '-2px',
+                zIndex: 2,
+                left: `calc(${timeOffset}% - 1px)`,
+                '&:before, &:after': {
+                  content: '""',
+                  display: 'block',
+                  position: 'absolute',
+                  left: '-2px',
+                  border: '3px solid',
+                  borderColor: 'red',
+                  borderLeftColor: 'transparent',
+                  borderRightColor: 'transparent',
+                },
+                '&:before': {
+                  top: '0',
+                  borderBottom: '0',
+                },
+                '&:after': {
+                  bottom: '0',
+                  borderTop: '0',
+                },
+              }}
+            />
+          </>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default ActiveHours;
+
+ActiveHours.propTypes = {
+  availableMonths: PropTypes.arrayOf(PropTypes.bool),
+  availableHours: PropTypes.arrayOf(PropTypes.bool),
+};
--- app/utils/data.js
+++ app/utils/data.js
@@ -3,18 +3,61 @@ export const ALL_MONTHS = Array.from({ length: 12 }, _ => true);
 
 export const parseAvailableMonths = (available, hemisphere = 'northern') => {
   if (available?.isAllYear) return ALL_MONTHS;
-  const monthDescription = available?.[`month-${hemisphere}`];
-  if (!monthDescription) return ALL_MONTHS;
-  return monthDescription.split('&').reduce((months, segment) => {
+  const monthsDescription = available?.[`month-${hemisphere}`];
+  if (!monthsDescription) return ALL_MONTHS;
+  return monthsDescription.split('&').reduce((months, segment) => {
     const newMonths = [...months];
-    const [from, to] = segment.trim().split('-');
+    const [from, to] = segment
+      .trim()
+      .split('-')
+      .map(m => parseInt(m, 10));
+    if (!to) {
+      newMonths[from - 1] = true;
+      return newMonths;
+    }
     const normalizedTo = from < to ? to : to + 12;
     Array.from(
       { length: normalizedTo - from + 1 },
-      (_, idx) => (from + idx) % 12,
+      (_, idx) => (from + idx - 1) % 12,
     ).forEach(m => {
       newMonths[m] = true;
     });
     return newMonths;
+  }, []);
+};
+
+// eslint-disable-next-line no-unused-vars
+export const ALL_HOURS = Array.from({ length: 24 }, _ => true);
+
+export const parseAvailableHours = available => {
+  if (available?.isAllDay) return ALL_HOURS;
+  const hoursDescription = available?.time;
+  if (!hoursDescription) return ALL_HOURS;
+  return hoursDescription.split('&').reduce((hours, segment) => {
+    const newHours = [...hours];
+    const [from, to] = segment
+      .trim()
+      .split('-')
+      .map(part => {
+        const trimmed = part.trim();
+        const h = parseInt(trimmed, 10);
+        if (trimmed.endsWith('am')) {
+          return h;
+        }
+        // pm
+        return h + 12;
+      });
+    if (!to) {
+      newHours[from - 1] = true;
+      return newHours;
+    }
+    const normalizedTo = from < to ? to : to + 24;
+    Array.from(
+      { length: normalizedTo - from + 1 },
+      (_, idx) => (from + idx) % 24,
+    ).forEach(m => {
+      newHours[m] = true;
+    });
+    return newHours;
   }, []);
 };
