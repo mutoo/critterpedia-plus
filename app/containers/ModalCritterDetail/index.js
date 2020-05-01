733ed2220393de8c64e906458bf4a1e2c7e75fa4
Lingjia Liu <gmutoo@gmail.com>
2020-05-01 10:27:07 +1000

    handle left/right key on detail modal

--- app/containers/ModalCritterDetail/index.js
+++ app/containers/ModalCritterDetail/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -16,17 +16,57 @@ import { HemisphereContext } from 'utils/contexts';
 import { CATEGORY_FISH, CATEGORY_INSECTS } from 'utils/const';
 import styles from './style.css';
 import selector from './selectors';
-import { name as key, reducer, closeCritterDetail } from './slice';
+import {
+  name as key,
+  reducer,
+  closeCritterDetail,
+  openCritterDetail,
+} from './slice';
 import DetailFish from './components/detail-fish';
 import DetailInsect from './components/detail-insect';
 
 function ModalCritterDetail() {
   useInjectReducer({ key, reducer });
-  const { isModalCritterDetailOpen, category, data, hemisphere } = useSelector(
-    selector,
-  );
+  const {
+    isModalCritterDetailOpen,
+    category,
+    data,
+    nextId,
+    prevId,
+    collection,
+    hemisphere,
+  } = useSelector(selector);
   const [thatHemisphere, setHemisphere] = useState(hemisphere);
   const dispatch = useDispatch();
+  useEffect(() => {
+    const onKeyDown = e => {
+      if (nextId !== prevId) {
+        switch (e.key) {
+          case 'ArrowRight':
+            dispatch(
+              openCritterDetail({
+                category,
+                id: nextId,
+                collection,
+              }),
+            );
+            break;
+          case 'ArrowLeft':
+            dispatch(
+              openCritterDetail({
+                category,
+                id: prevId,
+                collection,
+              }),
+            );
+            break;
+          default:
+        }
+      }
+    };
+    document.addEventListener('keydown', onKeyDown);
+    return () => document.removeEventListener('keydown', onKeyDown);
+  }, [nextId, prevId]);
   return (
     <Modal
       isOpen={isModalCritterDetailOpen}
