38570bda12b861686d225591f4cf2da2ed4c5dcc
Lingjia Liu <gmutoo@gmail.com>
2020-05-02 19:16:08 +1000

    add fish shadow preview

---
+++ app/pages/Critterpedia/components/list-view.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Flex, Box } from 'rebass';
+const ListView = ({ ...props }) => (
+  <Box {...props}>
+    <Flex></Flex>
+  </Box>
+);
+
+ListView.propTypes = {};
+
+export default ListView;
