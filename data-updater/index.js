6da2dbaeb57f938a244ee112d70f4ac46e53834d
Lingjia Liu <gmutoo@gmail.com>
2021-11-09 16:14:56 +1100

    fetch data from google sheets

---
+++ data-updater/index.js
@@ -0,0 +1,8 @@
+const { fetch } = require('./fetch');
+
+if (!process.env.GOOGLE_SHEET_API_KEY) {
+  console.error('Abort! google sheet api key is not set in ENV.');
+  process.exit(1);
+}
+
+fetch();
